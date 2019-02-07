ccf4c99f3bb0a76c1485b11aedd8a0d26a209639
mariggun <47255708+mariggun@users.noreply.github.com>
2019-02-08 01:57:15 +0900

    Add files via upload

--- color.js
+++ color.js
@@ -15,20 +15,25 @@ function BodySetBackgroundColor(color) {
 
 var Body = {
     setColor: function(color) {
-        document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+        // document.querySelector('body').style.color = color;
     },
     setBackgroundColor: function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
+        // document.querySelector('body').style.backgroundColor = color;
     }
 }
 var Links = {
     setColor: function(color) {
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while(i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
+        $('a').css('color', color); //3.html 에 구글 CDN 3.x snippet: 추가하면서 넣음
+        //$('a')는 웹 페이지에 있는 모든 <a> 태그를 jQuery로 제어 하겠다는 뜻
+
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length) {
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }
     }
 }
 
